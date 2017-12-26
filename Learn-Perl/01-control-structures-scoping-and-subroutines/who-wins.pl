cbfe44fabf63b8ed0172609222b4e2dfafecd1aa
yottacto <yotta.w.cto@gmail.com>
2017-12-26 20:01:27 +0800

    [@] hash and sort

--- Learn-Perl/01-control-structures-scoping-and-subroutines/who-wins.pl
+++ Learn-Perl/01-control-structures-scoping-and-subroutines/who-wins.pl
@@ -15,24 +15,24 @@ while (<$fh>) {
 }
 die "cannnot find 'Team $team_number'" if (eof $fh);
 
-my $max_bugs = -1;
-my $max_name;
+my %bugs;
+my $max_bugs = 0;
+my @max_name;
 while (my $line = <$fh>) {
     chomp($line);
-    next if $line eq "";
+    next if ($line eq "");
     last if ($line =~ m/^Team \d+$/);
-    $line =~ m/^(\w+): (\d+)$/;
-    my ($name, $bugs) = ($1, $2);
+    my ($name, $bugs) = split(/: +/, $line, 2);
     die "malformed '$line'" unless(defined $bugs);
+    $bugs{$name} = $bugs;
     if ($bugs > $max_bugs) {
         $max_bugs = $bugs;
-        $max_name = $name;
+        @max_name = $name;
+    } elsif ($bugs == $max_bugs) {
+        push(@max_name, $name)
     }
 }
 
-if ($max_bugs > 0) {
-    say "cong $max_name! Winner Winner Chicken Dinner!";
-} else {
-    say "Nobody fixed any bugs!";
-}
+say "$_: $bugs{$_}" for (sort keys %bugs);
+say "@max_name wins!";
 
